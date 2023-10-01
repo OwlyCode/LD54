1ecdaff8e8da724f7bfd47665683d059aa9a23a8
Tristan Maindron <tmaindron@gmail.com>
2023-10-01 19:12:12 +0200

    Web publication and fixes

--- Music.gd
+++ Music.gd
@@ -3,8 +3,7 @@ extends AudioStreamPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_parent().remove_child(self)
-	get_tree().root.add_child(self)
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
